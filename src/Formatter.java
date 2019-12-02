c95b051d34b0a35d0104afa7602b8409fb454099
Amodini Pathak <acpatha1@asu.edu>
2019-12-02 07:00:57 -0700

    All formatting commands implemented for single column. Still need to do double column. Also, debugging one last part of the single column justification with wrapping (one test case was causing an error notice to be printed).

--- src/Formatter.java
+++ src/Formatter.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 class Formatter {
 	private File importFile;
@@ -22,37 +23,124 @@ class Formatter {
 
 	String parse(JEditorPane importedText, JEditorPane formattedText) {
 		try {
-			BufferedReader bufferedReader = new BufferedReader(new FileReader(importFile));
+			//Read input file line by line and send text to importedText pane.
+			BufferedReader bufferedReaderInitial = new BufferedReader(new FileReader(importFile));
+			String lineforDisplay;
+			while ((lineforDisplay = bufferedReaderInitial.readLine()) != null) {
+				// Putting the text imported line by line into the JEditorPane
+				lineforDisplay += "\r"; //add new line
+				importedText.setText(importedText.getText() + lineforDisplay);
+			}
+			bufferedReaderInitial.close();
 			
-			int lineCount = 0;
+			BufferedReader bufferedReader = new BufferedReader(new FileReader(importFile));
 			String line;
 			String formattedLine;
-			String command;
-			while ((line = bufferedReader.readLine()) != null) {
+			int lineCount = 0;
+			boolean flag = false;
+			int numCol = 1;
+			while (!flag && (line = bufferedReader.readLine()) != null) {
 				lineCount++;
-				command = "";
-				formattedLine = "";
 				if (line.length() >= 1) {
-					if (line.substring(0, 1).compareTo("-") == 0) {
-						command = line;
-						String result = parseCommand(command, lineCount);
-						if (result.equals("Title")) {
-							//Not yet implemented
+					if (line.compareTo("-a2") == 0) {
+						numCol = 2;
+					} else if (line.compareTo("-a1") == 0) {
+						numCol = 1;
+					} else if (line.length() > 3 && (line.substring(0, 3).compareTo("-a2") == 0 
+							|| line.substring(0, 3).compareTo("-a1") == 0)) {
+						errorsReported += "Line " + lineCount + ": Invalid number of columns.\n";
+						numCol = 1;
+					}
+					if (numCol == 2) {
+						String doubleColumnSection;
+						if (line.compareTo("-a2") == 0) {
+							doubleColumnSection = "";
 						} else {
-							formattedLine = result;
+							doubleColumnSection = line + "\n";
 						}
-					} else {
-						formattedLine = applyFormatting(line);
+						int startLineCount = lineCount;
+						while ((line = bufferedReader.readLine()) != null && line.compareTo("-a1") != 0 
+								&& line.compareTo("-t") != 0 && !(line.length() > 3 && line.substring(0, 3).compareTo("-a1") == 0)
+								&& !(line.length() > 3 && line.substring(0, 3).compareTo("-a2") == 0)) {
+							lineCount++;
+							if (line.length() != 0) {
+								if (line.compareTo("-a2") != 0) {
+									doubleColumnSection += line + "\n";
+								}
+							}
+						}
+						formattedLine = applyDoubleColumnFormatting(doubleColumnSection, startLineCount + 1);
+						formattedText.setText(formattedText.getText() + formattedLine);
+						if (line == null) {
+							flag = true;
+						} else if (line.compareTo("-t") == 0) {
+							line = bufferedReader.readLine();
+							lineCount++;
+							if (line == null) {
+								flag = true;
+							} else {
+								int titleLength = line.length();
+								if (titleLength > 90) {
+									errorsReported += "Line " + lineCount + ": Title exceeds maximum number of characters.\n";
+								} else {
+									String underline = "";
+									for (int iterator = 0; iterator < titleLength; iterator++) {
+										underline += "-";
+									}
+									int numSpacesToCenter = 90 - titleLength;
+									int numSpacesToBegin = numSpacesToCenter/2;
+									int numSpacesToEnd = numSpacesToCenter - numSpacesToBegin;
+									for (int iterator = 0; iterator < numSpacesToBegin; iterator++) {
+										line = " " + line;
+										underline = " " + underline;
+									}
+									for (int iterator = 0; iterator < numSpacesToEnd; iterator++) {
+										line = line + " ";
+										underline = underline + " ";
+									}
+									formattedLine = line;
+									formattedText.setText(formattedText.getText() + formattedLine);
+									formattedText.setText(formattedText.getText() + underline);	
+									currentFormattingScheme.reset();
+									numCol = 1;
+								}
+							}
+						} else if (line.length() > 3 && (line.substring(0, 3).compareTo("-a2") == 0 || 
+								line.substring(0, 3).compareTo("-a1") == 0)) {
+							errorsReported += "Line " + lineCount + ": Invalid number of columns.\n";
+							numCol = 1;
+						} else if (line.compareTo("-a1") == 0) {
+							numCol = 1;
+						}
+					} else if (numCol == 1) {
+						String singleColumnSection = "";
+						if (line.compareTo("-a1") == 0) {
+							singleColumnSection = "";
+						} else {
+							singleColumnSection = line + "\n";
+						}
+						int startLineCount = lineCount;
+						while ((line = bufferedReader.readLine()) != null && line.compareTo("-a2") != 0) {
+							lineCount++;
+							if (line.length() != 0 && line.compareTo("-a1") != 0) {
+								if (line.length() > 3 && (line.substring(0, 3).compareTo("-a2") == 0 ||
+										line.substring(0, 3).compareTo("-a1") == 0)) {
+									errorsReported += "Line " + lineCount + ": Invalid number of columns.\n";
+								} else {
+									singleColumnSection += line + "\n";
+								}
+							}		
+						}
+						formattedLine = applySingleColumnFormatting(singleColumnSection, startLineCount + 1);
+						formattedText.setText(formattedText.getText() + formattedLine);
+						if (line == null) {
+							flag = true;
+						} else if (line.compareTo("-a2") == 0) {
+							numCol = 2;
+						}
 					}
 				}
-
-				// Putting the text imported line by line into the JEditorPane
-				line += "\r"; //add new line
-				importedText.setText(importedText.getText() + line);
-				formattedText.setText(formattedText.getText() + formattedLine);
-				//currently just copies everything from importedtext and writes it onto formattedText
 			}
-
 			// This removes the last \r\n we added, 3 since it also adds a blank space
 			importedText.getDocument().remove(importedText.getDocument().getLength() - 1, 3);
 			formattedText.getDocument().remove(formattedText.getDocument().getLength() - 1, 3);
@@ -61,6 +149,246 @@ class Formatter {
 		} catch (IOException | BadLocationException ignored) { }
 		return errorsReported;
 	}
+	
+	private String applySingleColumnFormatting(String line, int lineCount) {
+		String result = "";
+		Scanner section = new Scanner(line);
+		String currentLine;
+		int currentLineCount = lineCount - 1;
+		
+		while (section.hasNextLine() == true) {
+			currentLine = section.nextLine();
+			currentLineCount++;
+			if (currentLine.substring(0, 1).compareTo("-") == 0) {
+				String command = currentLine;
+				String returnedValue = parseCommand(command, lineCount);
+				if (returnedValue.equals("Title") && section.hasNextLine() == true) {
+					String title = section.nextLine();
+					currentLineCount++;
+					int titleLength = title.length();
+					if (titleLength > 90) {
+						errorsReported += "Line " + lineCount + ": Title exceeds maximum number of characters.\n";
+					} else {
+						String underline = "";
+						for (int iterator = 0; iterator < titleLength; iterator++) {
+							underline += "-";
+						}
+						int numSpacesToCenter = 90 - titleLength;
+						int numSpacesToBegin = numSpacesToCenter/2;
+						int numSpacesToEnd = numSpacesToCenter - numSpacesToBegin;
+						for (int iterator = 0; iterator < numSpacesToBegin; iterator++) {
+							title = " " + title;
+							underline = " " + underline;
+						}
+						for (int iterator = 0; iterator < numSpacesToEnd; iterator++) {
+							title = title + " ";
+							underline = underline + " ";
+						}
+						result = result + title + "\n";
+						result = result + underline + "\n";
+						currentFormattingScheme.reset();
+					}
+				} else {
+					result += returnedValue;
+				}
+			} else {
+				int desiredLineLength = currentFormattingScheme.getLineLength();
+				char desiredJustification = currentFormattingScheme.getJustification();
+				int desiredWrapping = currentFormattingScheme.getWrapping();
+				int desiredSpacing = currentFormattingScheme.getSpacing();
+				int desiredIndentation = currentFormattingScheme.getIndentation();
+				String nextLine = "";
+				currentLine += "\n";
+				
+				if(desiredIndentation != 0) {
+					for (int iterator = 0; iterator < desiredIndentation; iterator++) {
+						currentLine  = " " + currentLine;
+					}
+					currentFormattingScheme.setIndentation(0);
+				}
+				
+				if (desiredWrapping == 1) {
+					currentLine = currentLine.substring(0, currentLine.length() - 1);
+					boolean flag = false;
+					while (section.hasNextLine() && !flag) {
+						nextLine = section.nextLine();
+						currentLineCount++;
+						if (nextLine.substring(0, 1).compareTo("-") == 0) {
+							nextLine = nextLine.substring(0, nextLine.length() - 1);
+							flag = true;
+						} else {
+							currentLine = currentLine + " " + nextLine;
+						}
+					}
+					currentLine += "\n";
+				}
+
+				int numCharsInLine = currentLine.length() - 1;
+				
+				if (numCharsInLine > desiredLineLength) {
+					String tempLine = "";
+					while (currentLine.length() > desiredLineLength) {
+						int startIndex = 0;
+						int endIndex = desiredLineLength - 1;
+						while (currentLine.charAt(endIndex) != ' ') {
+							endIndex--;
+						}
+						tempLine += currentLine.substring(startIndex, endIndex + 1);
+						tempLine += "\n";
+						currentLine = currentLine.substring(endIndex + 1);
+					}
+					tempLine += currentLine;
+					currentLine = tempLine;
+				}
+				
+				if (desiredJustification == 'r') {
+					int startIndex = 0;
+					while (startIndex < currentLine.length()) {
+						int stopIndex = currentLine.indexOf('\n', startIndex);
+						String singleLine = currentLine.substring(startIndex, stopIndex);
+						int numSpacesToAdd = desiredLineLength - singleLine.length();
+						if (startIndex == 0) {
+							for (int iterator = 0; iterator < numSpacesToAdd; iterator++) {
+								currentLine = " " + currentLine;
+							}
+						} else {
+							String firstPart = currentLine.substring(0, startIndex);
+							String secondPart = currentLine.substring(startIndex);
+							String totalSpace = "";
+							for (int iterator = 0; iterator < numSpacesToAdd; iterator++) {
+								totalSpace += " ";
+							}
+							currentLine = firstPart + totalSpace + secondPart;
+						}
+						while (currentLine.charAt(startIndex) != '\n') {
+							startIndex++;
+						}
+						startIndex = startIndex + 1;
+					}
+				} else if (desiredJustification == 'c') {
+					int startIndex = 0;
+					while (startIndex < currentLine.length()) {
+						int stopIndex = currentLine.indexOf('\n', startIndex);
+						String singleLine = currentLine.substring(startIndex, stopIndex);
+						int numSpacesToAdd = desiredLineLength - singleLine.length();
+						int numSpacesToAddBegin = numSpacesToAdd/2;
+						int numSpacesToAddEnd = numSpacesToAdd - numSpacesToAddBegin;
+						if (startIndex == 0) {
+							for (int iterator = 0; iterator < numSpacesToAddBegin; iterator++) {
+								currentLine = " " + currentLine;
+								startIndex++;
+								stopIndex++;
+							}
+							String firstPart = currentLine.substring(0, stopIndex);
+							String lastPart = currentLine.substring(stopIndex);
+							String totalSpace = "";
+							for (int iterator = 0; iterator < numSpacesToAddEnd; iterator++) {
+								totalSpace += " ";
+								stopIndex++;
+							}
+							currentLine = firstPart + totalSpace + lastPart;
+						} else {
+							String firstPart = currentLine.substring(0, startIndex);
+							String middlePart = currentLine.substring(startIndex, stopIndex);
+							String lastPart = currentLine.substring(stopIndex);
+							String totalBeginSpace = "";
+							String totalEndSpace = "";
+							for (int iterator = 0; iterator < numSpacesToAddBegin; iterator++) {
+								totalBeginSpace += " ";
+								startIndex++;
+								stopIndex++;
+							}
+							for (int iterator = 0; iterator < numSpacesToAddEnd; iterator++) {
+								totalEndSpace += " ";
+								stopIndex++;
+							}
+							currentLine = firstPart + totalBeginSpace + middlePart + totalEndSpace + lastPart;
+						}
+						startIndex = stopIndex + 1;
+					}
+				} else if (desiredJustification == 'e') {
+					int startIndex = 0 + desiredIndentation;
+					while (startIndex < currentLine.length()) {
+						int stopIndex = currentLine.indexOf('\n', startIndex);
+						String singleLine;
+						if (startIndex == 0 + desiredIndentation) {
+							singleLine = currentLine.substring(0, stopIndex);
+						} else {
+							singleLine = currentLine.substring(startIndex, stopIndex);
+						}
+						int numSpacesToAdd = desiredLineLength - singleLine.length();
+						int numSpacesAdded = 0;
+						int firstInstanceOfSpace = currentLine.indexOf(' ', startIndex);
+						int iterator = firstInstanceOfSpace;
+						while (numSpacesAdded != numSpacesToAdd) {
+							String firstPart = currentLine.substring(0, iterator);
+							String secondPart = currentLine.substring(iterator);
+							currentLine = firstPart + " " + secondPart;
+							stopIndex++;
+							numSpacesAdded++;
+							while (currentLine.charAt(iterator) == ' ') {
+								iterator++;
+							}
+							iterator = currentLine.indexOf(' ', iterator);
+							if (iterator >= stopIndex - 1 || iterator == -1) {
+								iterator = firstInstanceOfSpace;
+							}
+						}
+						startIndex = stopIndex + 1;
+					}
+				}
+				
+				if (desiredSpacing == 2) {
+					int newLineIndex = 0;
+					while (newLineIndex < currentLine.length() - 2) {
+						newLineIndex = currentLine.indexOf('\n', newLineIndex + 2);
+						String firstHalf = currentLine.substring(0, newLineIndex);
+						String secondHalf = currentLine.substring(newLineIndex);
+						currentLine = firstHalf + "\n" + secondHalf;
+					}
+				}
+				
+				result += currentLine;
+				
+				if (nextLine.length() != 0) {
+					String returnedValue = parseCommand(nextLine, lineCount);
+					if (returnedValue.equals("Title") && section.hasNextLine() == true) {
+						String title = section.nextLine();
+						currentLineCount++;
+						int titleLength = title.length();
+						if (titleLength > 90) {
+							errorsReported += "Line " + lineCount + ": Title exceeds maximum number of characters.\n";
+						} else {
+							String underline = "";
+							for (int iterator = 0; iterator < titleLength; iterator++) {
+								underline += "-";
+							}
+							int numSpacesToCenter = 90 - titleLength;
+							int numSpacesToBegin = numSpacesToCenter/2;
+							int numSpacesToEnd = numSpacesToCenter - numSpacesToBegin;
+							for (int iterator = 0; iterator < numSpacesToBegin; iterator++) {
+								title = " " + title;
+								underline = " " + underline;
+							}
+							for (int iterator = 0; iterator < numSpacesToEnd; iterator++) {
+								title = title + " ";
+								underline = underline + " ";
+							}
+							result = result + title + "\n";
+							result = result + underline + "\n";
+							currentFormattingScheme.reset();
+						}
+					}
+				}	
+			}
+		}
+		section.close();
+		return result;
+	}
+	
+	private String applyDoubleColumnFormatting(String section, int lineCount) {
+		return "";
+		}
 	
 	private String parseCommand(String command, int lineCount) {
 		String result = "";
@@ -211,20 +539,6 @@ class Formatter {
 					}
 					break;
 		
-				case "-a":
-					if (command.length() != 3) {
-						errorsReported += "Line " + lineCount + ": Invalid number of columns.\n";
-						currentFormattingScheme.setNumColumns(1);
-					} else {
-						if (command.charAt(2) != '1' || command.charAt(2) != '2') {
-							errorsReported += "Line " + lineCount + ": Invalid number of columns.\n";
-							currentFormattingScheme.setNumColumns(1);
-						} else {
-							currentFormattingScheme.setNumColumns(Integer.parseInt(command.substring(2, 3)));
-						}
-					}
-					break;
-		
 				default:
 					errorsReported += "Line " + lineCount + ": Invalid command\n";
 					break;
@@ -232,12 +546,7 @@ class Formatter {
 		}
 		return result;
 	}
-	
-	private String applyFormatting(String line) {
-		String result = "";
-		
-		return result;
-	}
+
 	
 	class FormattingScheme {
 		//num char per line
@@ -252,9 +561,6 @@ class Formatter {
 		//1 = single, 2 = double
 		private int spacing;
 		
-		//number of columns
-		private int numColumns;
-		
 		//num spaces to indent by
 		private int indentation;
 		
@@ -264,7 +570,6 @@ class Formatter {
 			justification = 'l';
 			wrapping = 0;
 			spacing = 1;
-			numColumns = 1;
 			indentation = 0;
 		}
 		
@@ -300,14 +605,6 @@ class Formatter {
 			this.spacing = spacing;
 		}
 		
-		int getNumColumns() {
-			return numColumns;
-		}
-		
-		void setNumColumns(int numColumns) {
-			this.numColumns = numColumns;
-		}
-		
 		int getIndentation() {
 			return indentation;
 		}
@@ -321,7 +618,6 @@ class Formatter {
 			justification = 'l';
 			wrapping = 0;
 			spacing = 1;
-			numColumns = 1;
 			indentation = 0;
 		}
 	}
